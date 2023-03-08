6aaee7c9c60ea22f4a5260d80b825ec2d159020a
AjinkyaPradhan1 <virat@gmail.com>
2023-03-08 16:11:24 +0530

    1

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className='main'>
+        <div className='doraemon'>
+
+          <div className='face'></div>
+          <div className='inner_face'>
+            <div className='nose'></div>
+            <div className='strght_line'></div>
+            <div className='hor_line'></div>
+            <div className='mouth'></div>
+          </div>
+          <div className='eye_left'>
+            <div className='inner_eye_left'></div>
+          </div>
+          <div className='eye_right'>
+            <div className='inner_eye_right'></div>
+          </div>
+          
+
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
